ecac3514b4169fa23b1a1d655130f5dedab189df
Nilesh Chavan <nilesh.b.chavan@capgemini.com>
2020-05-03 23:57:01 +0800

    Updates

---
+++ precache-manifest.fef5159704dca9aad2334492f88e835c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbb94be0228ffd2303972730faace59",
+    "url": "/Face-recognition-frontend/index.html"
+  },
+  {
+    "revision": "c080b900b6cbea40a50a",
+    "url": "/Face-recognition-frontend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9c92cdd7fe86a0a5f2c2",
+    "url": "/Face-recognition-frontend/static/css/main.e2a5b1d9.chunk.css"
+  },
+  {
+    "revision": "c080b900b6cbea40a50a",
+    "url": "/Face-recognition-frontend/static/js/2.a9f189f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-recognition-frontend/static/js/2.a9f189f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c92cdd7fe86a0a5f2c2",
+    "url": "/Face-recognition-frontend/static/js/main.184468e9.chunk.js"
+  },
+  {
+    "revision": "1da0e01a90076df32264",
+    "url": "/Face-recognition-frontend/static/js/runtime-main.d75c12b0.js"
+  }
+]);
