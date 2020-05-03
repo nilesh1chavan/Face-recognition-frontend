626b87f032afba2c68b8a46dbfe58c2ae98af472
Nilesh Chavan <nilesh.b.chavan@capgemini.com>
2020-05-03 23:52:07 +0800

    gh pages changes updated

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
       this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('http://localhost:3000/findface', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
